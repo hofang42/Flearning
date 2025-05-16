432a4d3d131c6012347e4e79185882cf3ca54dbf
trongducdoan25 <trongducdoan25@gmail.com>
2025-05-16 21:42:57 +0700

    [FLN-3][Duc][UI] Add UI Hover Popup And Course #16.05.2025

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import PopupCard from "./components/common/Card/PopupCard";
 
 function App() {
+  const cardData = {
+    cardProps: {
+      image:
+        "https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=312&q=80",
+      category: "DEVELOPMENTS",
+      price: "$14.00",
+      title: "2021 Complete Python Bootcamp From Zero to Hero in Python",
+      rating: "5.0",
+      students: "265.7K",
+    },
+    detailedProps: {
+      title: "2021 Complete Python Bootcamp From Zero to Hero in Python",
+      author: "Kevin Gilbert",
+      authorAvatar: "https://randomuser.me/api/portraits/men/32.jpg",
+      rating: "5.0",
+      ratingCount: "357,914",
+      students: "265.7K",
+      level: "Beginner",
+      duration: "6 hour",
+      price: "$14.00",
+      oldPrice: "$26.00",
+      discount: "56% OFF",
+      learnList: [
+        "Learn to use Python professionally, learning both Python 2 and Python 3!",
+        "Create games with Python, like Tic Tac Toe and Blackjack!",
+        "Learn to use Python for data analysis and visualization!",
+      ],
+    },
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <PopupCard
+          cardProps={cardData.cardProps}
+          detailedProps={cardData.detailedProps}
+        />
+      </div>
     </div>
   );
 }
